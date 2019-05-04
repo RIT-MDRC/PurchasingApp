9a951080a0f6a93eab3840bcdb1639f4d3d1edea
Andrew Serra <andy@serra.us>
2019-05-03 22:58:20 -0400

    first commit text received from slack

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import abort, Flask, jsonify, request
+
+app = Flask(__name__)
+
+# FhcZWvkJkddwN9Y2peov53Sx
+
+def is_request_valid(request):
+    is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
+    is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+
+    return is_token_valid and is_team_id_valid
+
+@app.route('/purchase-item', methods=['POST'])
+def purchase():
+
+    LIST_INDEX_TEXT = 8
+    if not is_request_valid(request):
+        abort(400)
+
+    data = list(request.form.values())[LIST_INDEX_TEXT]
+
+    print(data[LIST_INDEX])
+
+    
+
+    return "success"
